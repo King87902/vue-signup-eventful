227b297c413b42bae31b01e0887290301f0fc73b
King87902 <kingbrizzy2022@gmail.com>
2025-02-20 02:52:53 +0100

    `Remove unused import and update alias resolution in vite.config.js`

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,9 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import path from 'path';
+
 import tailwindcss from '@tailwindcss/vite'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -21,7 +19,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': fileURLToPath(new URL('@/src', import.meta.url)),
     },
   },
 });
